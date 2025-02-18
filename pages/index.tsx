839abf19cf6cd82e736d1688ca721977ab186525
GustavoMont <lgmluisgm@gmail.com>
2025-02-17 21:37:36 -0300

    feat: create another banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,6 @@
+import { geistMono, geistSans } from "@/styles/fonts";
 import { TicketRange } from "@/util/html-generator";
-import localFont from "next/font/local";
 import { useState } from "react";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export default function Home() {
   const TICKETS_COUNT = 450;
